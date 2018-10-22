e0258ff0f1d85497d4a48b08478a74af66b5b9e5
Jeo <jeopatto@gmail.com>
2018-10-22 13:57:03 -0500

    Fix extension handlng

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,4 @@
-import {createStore, applyMiddleware, compose} from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
@@ -9,7 +9,7 @@ const store = createStore(
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
   )
 )
 
