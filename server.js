16175659bc677c7adbb6bdc10a94a0a776e1ce80
Jeo <jeopatto@gmail.com>
2018-10-22 15:20:37 -0500

    Static fix

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const passport = require('passport')
 const cors = require('cors')
 const path = require('path')
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(passport.initialize())
 app.use(cors())
@@ -17,32 +17,30 @@ const users = require('./users/routes')
 const posts = require('./posts/routes')
 const profiles = require('./profiles/routes')
 
+// DB Config
+const db = require('./config/keys').mongoURI
+
+// Connect DB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('> Database connected'))
+  .catch(err => console.log(err))
+
+// Use Routes
+app.use('/api/users', users)
+app.use('/api/profiles', profiles)
+app.use('/api/posts', posts)
+
 // If none of these routes are hit, serve static files
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'))
 
   // Send static files
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
   })
 }
-
-// DB Config
-const db = require('./config/keys').mongoURI
-
-// Connect DB
-mongoose
-  .connect(db, {useNewUrlParser: true})
-  .then(() => console.log('> Database connected'))
-  .catch(err => console.log(err))
-
-app.get('/', (req, res) => res.status(200).json('Working'))
-
-// Use Routes
-app.use('/api/users', users)
-app.use('/api/profiles', profiles)
-app.use('/api/posts', posts)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
