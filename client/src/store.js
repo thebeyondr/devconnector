0c2297b6aa9353449a8387bf70d157c2063207e2
Jeo <jeopatto@gmail.com>
2018-10-22 14:47:28 -0500

    Fix store

--- client/src/store.js
+++ client/src/store.js
@@ -4,10 +4,11 @@ import rootReducer from './reducers'
 
 const initialState = {}
 const middleware = [thunk]
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(
   rootReducer,
   initialState,
-  compose(applyMiddleware(...middleware))
+  composeEnhancers(applyMiddleware(...middleware))
 )
 
 export default store
